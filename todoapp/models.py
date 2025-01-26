1917b559e4459ff1a7f8edea29eea7f9b072d0b2
shauna-rsh <shaunarshanbhag@gmail.com>
2025-01-25 19:08:00 -0500

    Added task prioritization using ML

--- todoapp/models.py
+++ todoapp/models.py
@@ -5,3 +5,4 @@ class Task(models.Model):
     title = models.CharField(max_length=250)
     description = models.TextField(blank=True)
     completed = models.BooleanField(default=False)
+    priority = models.CharField(max_length=50, blank=True, null=True)
